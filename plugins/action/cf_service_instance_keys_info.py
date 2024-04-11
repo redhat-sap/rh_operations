a4c6e47399ed606b8a3767f314d387206398068e
kksat <22549266+kksat@users.noreply.github.com>
2024-04-11 12:21:03 +0000

    Release galaxy-1.24.0
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ plugins/action/cf_service_instance_keys_info.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Red Hat, Project Atmosphere
+#
+# Copyright 2024 Red Hat, Project Atmosphere
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, division, print_function
+import tempfile
+from ansible.errors import AnsibleActionFail
+
+__metaclass__ = type
+import json
+from ansible.plugins.action import ActionBase
+
+from ansible_collections.sap.sap_operations.plugins.action.cf_marketplace_info import (
+    CFClient,
+)
+
+
+def run_action(
+    client: CFClient,
+    service_instance_name: str,
+    service_instance_guid: str,
+    key_name: str,
+    key_guid: str,
+) -> dict:
+
+    rc = client.authenticate()
+    if rc.get("failed"):
+        return rc
+
+    rc, stdout, stderr = client.run_method(
+        path="/v3/service_instances",
+        method="GET",
+        headers={},
+        data={},
+    )
+
+    if rc or stderr:
+        return {"failed": True, "msg": stdout}
+
+    service_instances = json.loads(stdout)
+
+    if "errors" in service_instances:
+        return {
+            "failed": True,
+            "msg": "Error retrieving service instances",
+            "errors": service_instances["errors"],
+        }
+    service_instance = None
+
+    for si in service_instances["resources"]:
+        if service_instance_name and si["name"] == service_instance_name:
+            service_instance = si
+
+        if service_instance_guid and si["guid"] == service_instance_guid:
+            service_instance = si
+    data = dict(
+        service_instance_guid=service_instance["guid"],
+    )
+    rc, stdout, stderr = client.run_method(
+        path="/v2/service_keys",
+        method="GET",
+        headers={},
+        data=data,
+    )
+
+    if rc or stderr:
+        return {"failed": True, "msg": stdout}
+
+    service_instance_keys = json.loads(stdout)
+
+    if "errors" in service_instance_keys:
+        return {
+            "failed": True,
+            "msg": "Error retrieving service instance keys",
+            "errors": service_instance_keys["errors"],
+        }
+
+    return dict(
+        failed=False,
+        changed=False,
+        cf_service_instance_keys_info=service_instance_keys["resources"],
+    )
+
+
+class CFServiceInstanceKeysInfoModule(ActionBase):
+    _VALID_ARGS = frozenset(["username", "password", "api_endpoint"])
+    argument_spec = dict(
+        username=dict(type="str", required=False),
+        password=dict(type="str", required=False),
+        api_endpoint=dict(type="str", required=False),
+        service_instance_name=dict(type="str", required=False),
+        service_instance_guid=dict(type="str", required=False),
+        key_name=dict(
+            type="str", required=False, aliases=["service_instance_key_name"]
+        ),
+        key_guid=dict(
+            type="str", required=False, aliases=["service_instance_key_guid"]
+        ),
+    )
+    required_together = [
+        [
+            "username",
+            "password",
+            "api_endpoint",
+        ],
+    ]
+    mutually_exclusive = [
+        ["service_instance_name", "service_instance_guid"],
+        ["key_name", "key_guid"],
+    ]
+    required_one_of = [
+        [
+            "username",
+            "password",
+            "api_endpoint",
+        ],
+        ["service_instance_name", "service_instance_guid"],
+    ]
+
+    def __init__(
+        self, task, connection, play_context, loader, templar, shared_loader_obj
+    ):
+        """Initialize action module."""
+        super().__init__(
+            task, connection, play_context, loader, templar, shared_loader_obj
+        )
+
+    def run(self, tmp=None, task_vars=None):
+        validation_results, _ignore = self.validate_argument_spec(
+            argument_spec=self.argument_spec,
+            required_together=self.required_together,
+            mutually_exclusive=self.mutually_exclusive,
+            required_one_of=self.required_one_of,
+        )
+
+        if validation_results.error_messages:
+            raise AnsibleActionFail(
+                message="Validation failed: {0}".format(
+                    ", ".join(validation_results.error_messages)
+                ),
+            )
+        api_endpoint = self._task.args.get("api_endpoint")
+        username = self._task.args.get("username")
+        password = self._task.args.get("password")
+        service_instance_name = self._task.args.get("service_instance_name")
+        service_instance_guid = self._task.args.get("service_instance_guid")
+        key_name = self._task.args.get("key_name")
+        key_guid = self._task.args.get("key_guid")
+
+        with tempfile.TemporaryDirectory() as cf_home:
+            client = CFClient(
+                api_endpoint=api_endpoint,
+                username=username,
+                password=password,
+                cf_home=cf_home,
+            )
+            return run_action(
+                client,
+                service_instance_name=service_instance_name,
+                service_instance_guid=service_instance_guid,
+                key_name=key_name,
+                key_guid=key_guid,
+            )
+
+
+class ActionModule(CFServiceInstanceKeysInfoModule):
+    pass
---
+++ plugins/modules/cf_service_instance_keys_info.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Red Hat, Project Atmosphere
+#
+# Copyright 2024 Red Hat, Project Atmosphere
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+DOCUMENTATION = r"""
+---
+module: cf_service_instance_keys_info
+
+extends_documentation_fragment:
+  - sap.sap_operations.community
+  - sap.sap_operations.action_plugin
+  - sap.sap_operations.cloud_foundry
+
+author:
+  - Kirill Satarin (@kksat)
+
+short_description: Fetch information about Cloud Foundry service instance keys
+
+description:
+  - Fetch information about Cloud Foundry service instance keys
+
+version_added: 1.24.0
+
+options:
+  service_instance_guid:
+    description:
+      - The GUID of the Cloud Foundry service instance to fetch keys for.
+      - Either this or the service_instance_name parameter is required.
+    required: false
+    type: str
+  service_instance_name:
+    description:
+      - The name of the Cloud Foundry service instance to fetch keys for.
+      - Either this or the service_instance_guid parameter is required.
+    required: false
+    type: str
+"""
+
+EXAMPLES = r"""
+---
+- name: Fetch information about service instance keys
+  sap.sap_operations.cf_service_instance_keys_info:
+    username: user@email.domain
+    password: secret
+    api_endpoint: <cloud foundry api endpoint>
+    service_instance_guid: <service instance guid>
+
+- name: Fetch information about service instance keys
+  sap.sap_operations.cf_service_instance_keys_info:
+    username: user@email.domain
+    password: secret
+    api_endpoint: <cloud foundry api endpoint>
+    service_instance_name: <service instance name>
+"""
+
+RETURN = r"""
+---
+cf_service_instance_keys_info:
+  description: A list of service keys with their details.
+  returned: success
+  type: list
+  elements: dict
+  contains:
+    entity:
+      description: The entity details of the service key.
+      type: dict
+      contains:
+        credentials:
+          description: The credentials and related URLs for the service.
+          type: dict
+          contains:
+            endpoints:
+              description: Various service URLs.
+              type: dict
+              contains:
+                account_context_service_url:
+                  description: URL for the account context service.
+                  type: str
+                accounts_service_url:
+                  description: URL for the accounts service.
+                  type: str
+                cloud_automation_url:
+                  description: URL for the cloud automation service.
+                  type: str
+                entitlements_service_url:
+                  description: URL for the entitlements service.
+                  type: str
+                events_service_url:
+                  description: URL for the events service.
+                  type: str
+                external_provider_registry_url:
+                  description: URL for the external provider registry service.
+                  type: str
+                metadata_service_url:
+                  description: URL for the metadata service.
+                  type: str
+                order_processing_url:
+                  description: URL for the order processing service.
+                  type: str
+                provisioning_service_url:
+                  description: URL for the provisioning service.
+                  type: str
+                saas_registry_service_url:
+                  description: URL for the SaaS registry service.
+                  type: str
+            grant_type:
+              description: The grant type for authentication.
+              type: str
+            sap.cloud.service:
+              description: SAP cloud service identifier.
+              type: str
+            uaa:
+              description: Details about the UAA (User Account and Authentication).
+              type: dict
+              contains:
+                apiurl:
+                  description: API URL for the UAA service.
+                  type: str
+                clientid:
+                  description: Client ID for authentication.
+                  type: str
+                clientsecret:
+                  description: Client secret for authentication.
+                  type: str
+                identityzone:
+                  description: Identity zone for the UAA service.
+                  type: str
+                url:
+                  description: URL for the UAA service.
+                  type: str
+                verificationkey:
+                  description: Public verification key.
+                  type: str
+        name:
+          description: The name of the service key.
+          type: str
+        service_instance_guid:
+          description: GUID of the service instance.
+          type: str
+        service_instance_url:
+          description: URL of the service instance.
+          type: str
+        service_key_parameters_url:
+          description: URL for the service key parameters.
+          type: str
+    metadata:
+      description: Metadata related to the service key.
+      type: dict
+      contains:
+        created_at:
+          description: Creation timestamp of the service key.
+          type: str
+        guid:
+          description: GUID of the service key.
+          type: str
+        updated_at:
+          description: Last update timestamp of the service key.
+          type: str
+        url:
+          description: URL of the service key.
+          type: str
+"""
