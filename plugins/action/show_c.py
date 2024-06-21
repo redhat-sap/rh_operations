c3fd4d2fabcada41e4ccb289e44cea7ab7f50dae
kksat <22549266+kksat@users.noreply.github.com>
2024-06-21 13:49:55 +0000

    Release galaxy-1.31.0
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ plugins/action/show_c.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Red Hat, Project Atmosphere
+#
+# Copyright 2024 Red Hat, Project Atmosphere
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, division, print_function
+from datetime import date
+
+__metaclass__ = type
+
+from ansible.utils.display import Display
+from ansible.plugins.action import ActionBase
+
+COPYRIGHT = f"""
+Ansible collection for sap automation
+Copyright (C) 2022-{date.today().strftime("%Y")} Project Atmosphere
+See CONTRIBUTORS
+
+This program comes with ABSOLUTELY NO WARRANTY; for details see `show w' statement.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
+"""
+
+
+def run_action() -> dict:
+    Display().display(COPYRIGHT)
+    return {
+        "changed": False,
+        "failed": False,
+        "msg": "Success",
+    }
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        return run_action()
